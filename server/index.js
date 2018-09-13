ca5715cc5be2a176f80b261a7572a4435ce05601
masoud Eyvatvandy <masoud.ey@gmail.com>
2018-09-13 23:29:04 +0430

    in progress

--- server/index.js
+++ server/index.js
@@ -2,5 +2,22 @@ import 'babel-polyfill';
 import 'dotenv/config';
 import path from "path";
 import http from "http";
+import https from "https";
 import cors from "cors";
+import bodyParser from "body-parser";
 import express from "express";
+import jwt from "jsonwebtoken";
+import React from "react";
+import { renderToString, renderToStaticMarkup } from "react-dom/server";
+import { StaticRouter, matchPath } from "react-router-dom";
+import { ApolloServer } from 'apollo-server-express';
+import { AuthenticationError } from 'apollo-server';
+import { ApolloProvider, getDataFromTree } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import mongoose from 'mongoose';
+import { Helmet } from 'react-helmet';
+
+
+
