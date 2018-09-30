ecf4afe6797a7b2aafbda6655c2e432bf2dda9f1
masoud Eyvatvandy <masoud.ey@gmail.com>
2018-09-30 21:47:50 +0330

    in progress

--- client/index.js
+++ client/index.js
@@ -10,7 +10,8 @@ import { WebSocketLink } from "apollo-link-ws";
 import { ApolloLink, split } from "apollo-link";
 import { withClientState } from "apollo-link-state";
 import { onError } from "apollo-link-error";
-import {  } from "";
+import * as Cookies from "es-cookie";
+import { createBrowserHistory } from "history";
 
 import App from "../shared/app";
 import registerServiceWorker from './registerServiceWorker';
@@ -38,9 +39,70 @@ const terminatingLink = split(
 );
 
 const authLink = new ApolloLink((operation, forward) => {
-    operation.setContext(({ headers = {} }) => ({
-        headers: {
-            ...headers,
+    operation.setContext(({ cookies = {} }) => ({
+        cookies: {
+            ...cookies,
+            'x-token': Cookies.get('token'),
         },
-    }))
+    }));
+
+    return forward(operation);
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+        graphQLErrors.forEach(({ message, locations, path }) => {
+            console.log('graphQL error', message);
+
+            if (message === 'NOT_AUTHENTICATED') {
+                
+            }
+        });
+    }
+
+    if (networkError) {
+        console.log('Network error', networkError);
+
+        if (networkError.statusCode === 401) {
+
+        }
+    }
+});
+
+const cache = new InMemoryCache().restore(window.__APOLLO_STATE__);
+
+const defaults = {
+
+}
+
+const resolvers = {
+
+}
+
+const stateLink = withClientState({
+    cache,
+    defaults,
+    resolvers,
 })
+
+const link = ApolloLink.from([stateLink, authLink, errorLink, terminatingLink]);
+
+
+const client = new ApolloClient({
+    link,
+    cache,
+});
+
+const history = createBrowserHistory();
+const root = document.getElementById('root');
+
+hydrate(
+    <ApolloProvider client={client} >
+        <BrowserRouter history={history}>
+            <App />
+        </BrowserRouter>
+    </ApolloProvider>,
+    root
+);
+
+registerServiceWorker();
