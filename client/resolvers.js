2fbce4738f71cc8c856fae17841a2fc4f6257e03
masoud Eyvatvandy <masoud.ey@gmail.com>
2018-10-05 18:16:41 +0330

    in progress

--- client/resolvers.js
+++ client/resolvers.js
@@ -9,7 +9,34 @@ export const defaults = {
 export const resolvers = {
     Mutation: {
         signUp: (_, { username, email, password, firstName, lastName, userImage }, { cache }) => {
-            
+            const query = gql`
+                {
+                    users @client {
+                        username
+                        email
+                        password
+                        firstName
+                        lastName
+                        userImage
+                    }
+                }
+            `;
+            const { users } = cache.readQuery({ query });
+            const newData = {
+                username,
+                email,
+                password,
+                firstName,
+                lastName,
+                userImage,
+                __typename: "User",
+            },
+
+            const data = {
+                users: users.concat([newData])
+            };
+            cache.writeData({ data });
+            return newData;
         }
     }
 }
