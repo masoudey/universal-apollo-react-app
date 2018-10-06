31f2a592927f5f1d71a702db7297bd3f437237ad
masoud Eyvatvandy <masoud.ey@gmail.com>
2018-10-06 22:53:19 +0330

    in progress

---
+++ shared/app.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from "react";
+import { withRouter } from "react-router-dom";
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Fragment>
+                
+            </Fragment>
+        )
+    }
+}
+
+export default App;
