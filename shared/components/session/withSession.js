76069b113c2aa191337e9067ea2639e2e9cd7705
masoud Eyvatvandy <masoud.ey@gmail.com>
2018-10-13 21:52:06 +0330

    in progress

--- shared/components/session/withSession.js
+++ shared/components/session/withSession.js
@@ -1,3 +1,11 @@
 import React from "react";
 import { Query } from "react-apollo";
 import { GET_CURENT_USER } from "../../queries/userQueries";
+
+const withSession = Component => props => (
+    <Query query={GET_CURENT_USER} >
+        {({ data, refetch }) => (
+            <Component  {...props} session={data} refetch={refetch}/>
+        )}
+    </Query>
+)
