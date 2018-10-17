6aa7e1d6cb201707544cbf2c94f120ff955f3f94
masoud Eyvatvandy <masoud.ey@gmail.com>
2018-10-17 17:38:40 +0330

    in progress

--- shared/app.js
+++ shared/app.js
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from "react";
 import { withRouter } from "react-router-dom";
+import withSession from "./components/session/withSession";
+
+import Routes from "./routes";
 
 
 class App extends Component {
@@ -8,12 +11,15 @@ class App extends Component {
     }
 
     render() {
+        const session = this.props.session;
         return (
             <Fragment>
-                
+                <main>
+                    <Routes session={session}/>
+                </main>
             </Fragment>
         )
     }
 }
 
-export default App;
+export default withRouter(withSession(App));
---
+++ shared/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { withRouter, NavLink, Link } from "react-router-dom";
+
+class Header extends Component {
+
+    render() {
+        return (
+            <header>
+                
+            </header>
+        )
+    }
+}
